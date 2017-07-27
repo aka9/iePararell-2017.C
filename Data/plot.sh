384152a6cda9af023d9f5acba896462099ba4850
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2017-07-28 03:23:50 +0900

    add title to plot.sh

--- Data/plot.sh
+++ Data/plot.sh
@@ -5,6 +5,7 @@ file='CmpareExecutionTime'
 gnuplot<<PLOT
 set xlabel "Prime Range"
 set ylabel "Execution Time"
+set title "Cmpare Execution Time"
 set terminal svg
 set output "CmpareExecutionTime.svg"
 
